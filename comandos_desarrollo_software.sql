eca50cbca7e6cbf3e63a9f7b630eee300ac771f1
Arturo Mata <artmata22@outlook.com>
2023-07-22 21:40:59 -0600

    paso 1 development

--- comandos_desarrollo_software.sql
+++ comandos_desarrollo_software.sql
@@ -3,3 +3,5 @@ CREATE TABLE preguntas
 me pregunto cuanto tiempo tardaré en encontrar trabajo xD
 
 Quiero que se agregue otro puntito en el arbol creado
+
+tenemos que contemplar el paso 1 de devopment
