8815d83f4afe1c5ecaedc9bea53408c2d0e5218a
Arturo Mata <artmata22@outlook.com>
2023-07-22 21:45:53 -0600

    paso 2 development

--- comandos_desarrollo_software.sql
+++ comandos_desarrollo_software.sql
@@ -5,3 +5,5 @@ me pregunto cuanto tiempo tardaré en encontrar trabajo xD
 Quiero que se agregue otro puntito en el arbol creado
 
 tenemos que contemplar el paso 1 de devopment
+
+paso 2 development
