0d247189d1eae8b8be1fe81ba4af70d7f95a858e
Arturo Mata <artmata22@outlook.com>
2023-07-24 17:08:23 -0600

    funcion am

--- comandos_desarrollo_software.sql
+++ comandos_desarrollo_software.sql
@@ -7,3 +7,4 @@ Quiero que se agregue otro puntito en el arbol creado
 tenemos que contemplar el paso 1 de devopment
 
 paso 2 development
+Probando siu funciona el comando en git de commit -am para hacer add y commit a la vez
