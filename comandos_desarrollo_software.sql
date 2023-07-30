b5371fc7c38d9b4085ca657681b9afaadfdc4f6e
Arturo Mata <artmata22@outlook.com>
2023-07-29 21:08:19 -0600

    Feature 1

--- comandos_desarrollo_software.sql
+++ comandos_desarrollo_software.sql
@@ -7,4 +7,5 @@ Quiero que se agregue otro puntito en el arbol creado
 tenemos que contemplar el paso 1 de devopment
 
 paso 2 development
-Probando siu funciona el comando en git de commit -am para hacer add y commit a la vez
+Probando siu funciona el comando en git de commit -am para hacer add y commit a la vez.
+Esta modificacion se hizo en branch feature 1
