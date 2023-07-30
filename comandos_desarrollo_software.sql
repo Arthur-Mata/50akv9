c71da243c78d8487b147a73120c52ac43c72328f
Arturo Mata <artmata22@outlook.com>
2023-07-29 21:26:54 -0600

    main 2 real

--- comandos_desarrollo_software.sql
+++ comandos_desarrollo_software.sql
@@ -1,7 +1,7 @@
 CREATE TABLE preguntas
 
 me pregunto cuanto tiempo tardaré en encontrar trabajo xD
-En el branch main se hizo esa modificacion 1
+En el branch main se hizo esa modificacion 1 y esto es la 2
 Quiero que se agregue otro puntito en el arbol creado
 
 tenemos que contemplar el paso 1 de devopment
